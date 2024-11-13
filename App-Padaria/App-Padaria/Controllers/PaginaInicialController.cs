07d30bfe662fc8c6fa87c05d61935bee6249cf09
Lincoln <lincoln.sf11@gmail.com>
2024-11-12 22:11:00 -0300

    Primeiros arquivos

---
+++ App-Padaria/App-Padaria/Controllers/PaginaInicialController.cs
@@ -0,0 +1,34 @@
+﻿using App_Padaria.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace App_Padaria.Controllers
+{
+    public class PaginaInicialController : Controller
+    {
+        
+        private readonly ILogger<PaginaInicialController> _logger;
+
+        public PaginaInicialController(ILogger<PaginaInicialController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+    }
+}
---
+++ App-Padaria/App-Padaria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace App_Padaria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
