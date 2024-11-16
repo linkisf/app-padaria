dc0945712fd239774fbd8e59a3120892a8cd0771
Lincoln <lincoln.sf11@gmail.com>
2024-11-15 21:16:12 -0300

    Estilo menu navegacao + paginas principais

---
+++ App-Padaria/App-Padaria/Controllers/ContabilidadeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App_Padaria.Controllers
+{
+    public class ContabilidadeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
