dbaf82abee6c5f854f0e5ae6991e108d57097eb3
Lincoln <lincoln.sf11@gmail.com>
2024-11-17 13:14:35 -0300

    Botao voltar

--- App-Padaria/App-Padaria/wwwroot/js/site.js
+++ App-Padaria/App-Padaria/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+
+    $(".btn-voltar").on("mousedown", function () {        
+        $('.svg-btn-voltar').css("background-color", "#5C5327");
+        $('.svg-btn-voltar path').css("fill", "#E1CC61");
+    }).on("mouseup", function () {
+        $('.svg-btn-voltar').css("background-color", "#E1CC61");
+        $('.svg-btn-voltar path').css("fill", "#5C5327");
+    });
+
+    $('.btn-voltar').on('click', function (e) {
+        e.preventDefault();
+        window.history.back();
+    });
+
+
+
+});
